9136c91b9adbcb0931acb56710f415c904df5940
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-02-20 23:28:59 +0200

    added routes, controllers, models, services

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,18 +15,34 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api/', router);
 
-const start = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useStrictQuery: true,
-    });
-    app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
-};
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-start();
+app.listen(5000, () => console.log(`Server started on PORT ${PORT}`));
+
+// const start = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       // useCreateIndex: true,
+//       useUnifiedTopology: true,
+//       //useFindAndModify: false,
+//       //useStrictQuery: false,
+//     });
+//     //mongoose.set('strictQuery', true);
+//     app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+//start();
---
+++ server/service/mail-service.js
@@ -0,0 +1,65 @@
+// const nodemailer = require('nodemailer');
+
+// class MailService {
+//   constructor() {
+//     this.trasporter = nodemailer.createTransport({
+//       host: 'smtp.gmail.com',
+//       port: process.env.SMTP_PORT,
+//       secure: false,
+//       auth: {
+//         user: process.env.SMTP_USER,
+//         password: process.env.SMTP_PASSWORD,
+//       },
+//     });
+//   }
+
+//   async sendActivationMail(to, link) {
+//     await this.trasporter.sendMail({
+//       from: process.env.SMTP_USER,
+//       to,
+//       subject: 'Activation of account' + process.env.API_URL,
+//       text: '',
+//       html: `
+//                 <div>
+//                     <h1>For actication click link</h1>
+//                     <a href="${link}">${link} </a>
+//                 </div>
+//             `,
+//     });
+//   }
+// }
+
+// module.exports = new MailService();
+
+const nodemailer = require('nodemailer');
+
+class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'maxim.burinskyi@gmail.com',
+        pass: 'Vjybnjhbyu12',
+      },
+    });
+  }
+
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: 'Активация аккаунта на ' + process.env.API_URL,
+      text: '',
+      html: `
+                    <div>
+                        <h1>Для активации перейдите по ссылке</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `,
+    });
+  }
+}
+
+module.exports = new MailService();
