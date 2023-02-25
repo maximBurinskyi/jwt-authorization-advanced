414a00538827ccdcbf79c05acb9ae5cdde80ee2e
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-02-25 23:47:13 +0200

    added logout and refresh

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -4,7 +4,7 @@ const tokenModel = require('../models/token-model');
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '30m',
+      expiresIn: '30s',
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
       expiresIn: '30d',
@@ -14,6 +14,25 @@ class TokenService {
       refreshToken,
     };
   }
+
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
   async saveToken(userId, refreshToken) {
     const tokenData = await tokenModel.findOne({ user: userId });
     if (tokenData) {
@@ -22,6 +41,16 @@ class TokenService {
     }
     const token = await tokenModel.create({ user: userId, refreshToken });
     return token;
+  }
+
+  async removeToken(refreshToken) {
+    const tokenData = await tokenModel.deleteOne({ refreshToken });
+    return tokenData;
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await tokenModel.findOne({ refreshToken });
+    return tokenData;
   }
 }
 
